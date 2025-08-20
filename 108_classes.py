02811c8745742716641a42a629da152e5c69bdca
athul <athular.me@gmail.com>
2025-08-21 03:58:39 +0530

    adding

--- 108_classes.py
+++ 108_classes.py
@@ -4,5 +4,18 @@ class Car:
         self.year=year
         self.color=color
         self.for_sale=for_sale
+    #using methods 
+    def drive(self):
+        print(f"you are driving {self.model}")
+    def stop(self):
+        print(f"you are braking {self.model}")
 car1=Car("Tesla",2025,"Red",False)
+car2=Car("BMW",2015,"White",True)
+
+
 print(car1.color)
+print(car1.model)
+print(car2.year)
+print(car2.for_sale)
+car1.drive()
+car2.stop()
